d732a6f8047b600988147fa545c1a9c28fa47f9b
Abdulla Alaydaroos <abalayda@lab1r2s9.42abudhabi.ae>
2026-08-20 10:42:07 +0400

    ex03

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abalayda <abalayda@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/20 10:06:53 by abalayda          #+#    #+#             */
+/*   Updated: 2026/08/20 11:22:12 by abalayda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0 || nb == 1)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
