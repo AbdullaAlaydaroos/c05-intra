8b0b88ca5b8fb6ab2a65e9c03e9e9292b5880af7
Abdulla Alaydaroos <abalayda@lab1r2s9.42abudhabi.ae>
2026-08-20 11:16:07 +0400

    ex03: add main

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -18,3 +18,42 @@ int	ft_recursive_power(int nb, int power)
 		return (1);
 	return (nb * ft_recursive_power(nb, power - 1));
 }
+
+/*
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power);
+
+int	main(void)
+{
+	int	i;
+	int	j;
+
+	i = -5;
+	while (i < 0)
+	{
+		j = -1;
+		printf("%d ^ %d: %d\n", i, j, ft_recursive_power(i, j));
+		j++;
+		while (j < 4)
+		{
+			printf("%d ^  %d: %d\n", i, j, ft_recursive_power(i, j));
+			j++;
+		}
+		i++;
+	}
+	while (i < 6)
+	{
+		j = -1;
+		printf(" %d ^ %d: %d\n", i, j, ft_recursive_power(i, j));
+		j++;
+		while (j < 4)
+		{
+			printf(" %d ^  %d: %d\n", i, j, ft_recursive_power(i, j));
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+*/
